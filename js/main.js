ea75a8b8b44fe0d83d0fb6c94d4389b715c9f366
Elliott Soper <elliott.soper@gmail.com>
2016-03-19 14:05:09 -0400

    Added scrollTo JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-$(document).on('ready', function(){
+$(document).on("ready", function(){
+	var navHeight = $(".nav-wrapper").outerHeight(true);
 
-	var navHeight = $('.nav-wrapper').outerHeight(true);
+	$("body").css("margin-top", navHeight);
 
-	$('body').css('margin-top', navHeight);
+
+
+	$(".nav-main a").on("click", function(e){
+		console.log(e);
+		e.preventDefault();
+
+		$('.nav-main a').removeClass("is-current");
+		$(this).addClass("is-current");
+
+		console.log($(this).attr("href"));
+		var whereToScroll = $(this).attr("href");
+		
+		$.scrollTo(whereToScroll, {
+			duration: 800,
+			offset: -navHeight,
+		});
+	});
+});
